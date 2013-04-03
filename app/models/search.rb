980dc7f1221d516516861829131227cbb224ce3f
Raphael Weiner <raphael.weiner@gmail.com>
2013-04-03 12:07:09 -0600

    updated search

--- app/models/search.rb
+++ app/models/search.rb
@@ -24,13 +24,27 @@ module Search
     end
   end
 
+# Model.where(
+#     'created_at >= :five_days_ago or updated_at >= :three_days_ago',
+#     :five_days_ago  => Time.now - 5.days,
+#     :three_days_ago => Time.now - 3.days
+# )
+
   def self.filter_admin_orders(params={})
     order = Order.includes(:user)
     if params[:status].present? && params[:status] != 'all'
       order = order.where(status: params[:status])
     end
     if params[:date_symbol].present? && params[:date].present?
-      order = order.where("orders.created_at #{params[:date_symbol]} ?", params[:date])
+      date = params[:date].split('-').map{ |char| char.to_i }
+      if params[:date_symbol] == '<' || params[:date_symbol] == '>'
+        order = order.where("orders.created_at #{params[:date_symbol]}= ?",
+                            params[:date])
+      elsif params[:date_symbol] == '='
+        order = order.where("orders.created_at >= ? and orders.created_at <= ?",
+          DateTime.new(date[0], date[1], date[2]),
+          DateTime.new(date[0]+1, date[1], date[2]))
+      end
     end
     if params[:email].present?
       order = order.where("email = ?", params[:email])
