a49478ae492a0951e0f4a8ece41ff77c7a7f6a85
Raphael Weiner <raphael.weiner@gmail.com>
2013-04-03 20:11:50 -0600

    Pull out mail config from environments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,24 +60,4 @@ StoreEngine::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-
-  # Log the query plan for queries taking more than this (works
-  # with SQLite, MySQL, and PostgreSQL)
-  # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  config.action_mailer.default_url_options = { :host => 'http://franks-monsterporium.herokuapp.com/' }
-  # ActionMailer Config
-  # Setup for production - deliveries, no errors raised
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default :charset => "utf-8"
-  ActionMailer::Base.smtp_settings = {
-      :port =>           '587',
-      :address =>        'smtp.mandrillapp.com',
-      :user_name =>      ENV['MANDRILL_USERNAME'],
-      :password =>       ENV['MANDRILL_APIKEY'],
-      :domain =>         'heroku.com',
-      :authentication => :plain
-  }
-  ActionMailer::Base.delivery_method = :smtp
 end
