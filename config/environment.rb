5d71a731030e6b61c992fa4f760b1225058fdb91
Raphael Weiner <raphael.weiner@gmail.com>
2013-04-03 20:22:47 -0600

    change mail config files

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,6 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-
 StoreEngine::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
