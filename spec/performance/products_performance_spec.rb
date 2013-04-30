342b236458b8b2fa1bd757c3324d4163eacdac70
Jeff Casimir <jeff@casimircreative.com>
2013-04-30 11:15:07 -0700

    Got a first spec running

--- spec/performance/products_performance_spec.rb
+++ spec/performance/products_performance_spec.rb
@@ -7,7 +7,13 @@ describe "Products Performance", :type => :feature do
     it "loads the products listing" do
       repetitions.times do
         visit '/products'
+        # Page 1 is Visible
         expect(page).to have_content('Bobby 0')
+        expect(page).to have_content('The Great Fairy 1')
+        
+        # Other pages are not visible
+        expect(page).to_not have_content('The Great Slump 1')
+        expect(page).to_not have_content('Ruby 2')
       end
     end
   end
